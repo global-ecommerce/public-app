463e882bc1d482955f285bfbf0a5fb10c55b6a11
chris <44230073+global-ecommerce@users.noreply.github.com>
2019-03-19 20:00:12 +0530

    Update application.py

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ def new_handle_request_html():
         return "Internal Server Error\n", 500
     else:
         c.labels(http_code = '200').inc()
-        time.sleep(0.005)
+        #time.sleep(0.005)
         return "Hello World!\n"
 
 start_http_server(8000)
