c34e7c08708ac049ac101a822194669f272474ed
chris <44230073+global-ecommerce@users.noreply.github.com>
2019-10-18 09:18:14 +0530

    Update application.py

--- application.py
+++ application.py
@@ -11,11 +11,11 @@ c = Counter('requests', 'Number of requests served, by http code', ['http_code']
 
 @app.route('/')
 def hello():
-    return handle_request_html()
+    return new_handle_request_html()
 
 # http request handler for all
 def handle_request_html():
-    os.environ['SUCCESS_RATE'] = "40"
+    os.environ['SUCCESS_RATE'] = "90"
     if randrange(1, 100) > int(os.environ['SUCCESS_RATE']):
         c.labels(http_code = '500').inc()
         return "Internal Server Error\n", 500
@@ -26,7 +26,7 @@ def handle_request_html():
 
 # New http handler for all fg
 def new_handle_request_html():
-    os.environ['SUCCESS_RATE'] = "90"
+    os.environ['SUCCESS_RATE'] = "40"
     if randrange(1, 100) > int(os.environ['SUCCESS_RATE']):
         c.labels(http_code = '500').inc()
         return "Internal Server Error\n", 500
